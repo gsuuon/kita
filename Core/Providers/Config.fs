49b74b18af6da1eb697fc559109d43e2327477ad
Steven Sun <gsuuon+dev@gmail.com>
2021-03-01 14:40:24 -0500

    Move Defaults into Config (temporary)

--- Core/Providers/Config.fs
+++ Core/Providers/Config.fs
@@ -3,3 +3,12 @@ namespace Kita.Core.Providers
 type Config =
   { name : string
     deploy : string }
+
+module Default =
+    let Local =
+      { name = "Local"
+        deploy = "deploy" }
+
+    let Azure =
+      { name = "Azure"
+        deploy = "func host start" }
