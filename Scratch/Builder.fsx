bd0b35e9dc9cb6468945bbca96b55c33a29b5fd3
Steven Sun <gsuuon+dev@gmail.com>
2021-03-01 12:54:43 -0500

    Add Scratch exploratory files

---
+++ Scratch/Builder.fsx
@@ -0,0 +1,53 @@
+type Request = { content : string }
+
+let someFunction x = printfn "Got %A" x
+
+type ScratchBuilder() =
+    member _.Bind (m, f) = f m
+    member _.For (m, f) = f m
+    member _.Return x = x
+    member _.Yield x = x
+    (* member _.Combine (ma, mb) = mb *)
+    (* member _.Delay f = f() *)
+    (* member _.Run f = f() *)
+
+    [<CustomOperation("test", MaintainsVariableSpace=true)>] // MaintainsVariableSpace keeps variables usable after the operator - the return type is the same as the input type
+    member _.Test (ctx, [<ProjectionParameter>]a) =
+        // ProjectionParameter attribute modifies the passed-in argument to be a function that takes the
+        // first arg in order to bind cexpr scope variables
+        // I assumed ctx here was M<'T> but it must be something else, since here M<'T> is just 'T
+        // I think ctx is a tuple of all variables in the cexpr scope, the generated function grabs the necessary values from it
+        // to pull into the expression scope
+        someFunction <| (a ctx)()
+
+        ctx
+
+    [<CustomOperation("test2", MaintainsVariableSpace=true)>]
+    member _.Test2 (ctx, [<ProjectionParameter>]a) =
+        someFunction <| a ctx
+
+        ctx
+
+    // NOTE CustomOperation overloaded methods are coming:
+    // https://github.com/fsharp/fslang-design/blob/master/preview/FS-1056-allow-custom-operation-overloads.md
+
+    // More info about the the attribute properties starting page 71 of the F# language spec
+    // https://fsharp.org/specs/language-spec/4.1/FSharpSpec-4.1-latest.pdf
+
+let scratch = ScratchBuilder()
+
+scratch {
+    let! x = 1
+
+    let reqA = { content = "hi" }
+    let reqB = { content = "bye" }
+
+    test (fun _ -> reqA.content)
+    test (fun _ -> reqB.content)
+
+    test2 x
+
+    test (fun _ -> reqA.content + reqB.content)
+    let y = 0
+    return y
+}
---
+++ Scratch/StateBuilder.fsx
@@ -0,0 +1,75 @@
+type OtherType = { foo: int }
+
+type State<'state, 'a> = State of ('state -> 'a * 'state)
+
+[<AutoOpen>]
+module State =
+    let run (State f) s = f s
+    let ret x = State (fun s -> x, s)
+
+    type Builder() =
+        member inline _.Zero () =
+            State (fun s -> Unchecked.defaultof<_>, s)
+
+        member _.Bind (State m: State<int, int>,
+                    f: int -> State<int,int>) =
+            State (fun s ->
+                let (x, s') = m s
+                let (State m') = f x
+                printfn "int bind"
+                m' s' )
+
+        member _.Bind (State m, f) =
+            State (fun s ->
+                let (x, s') = m s
+                let (State m') = f x
+                printfn "normal bind"
+                m' s' )
+
+        member _.Bind (o: OtherType, f) =
+            State (fun s ->
+                let (State m) = ret o
+                let (x, s') = m s
+                let (State m') = f x
+                printfn "other bind"
+                m' s' )
+
+        member _.Return (x) =
+            ret x
+
+        member _.Delay f =
+            State (fun s ->
+                let (State f) = f()
+                f s )
+
+        member _.ReturnFrom m =
+            m
+
+        member _.Yield x = ret x
+
+        [<CustomOperation("custom", MaintainsVariableSpaceUsingBind=true)>]
+        member _.Custom (State runner, [<ProjectionParameter>]argA) =
+            State (fun s ->
+                let (ctx, s) = runner s
+                let myArgA = argA ctx
+                printfn "Got arga: %A" myArgA
+                ctx, s
+            )
+
+    let state = Builder()
+
+    let getEnv = State (fun s -> s, s)
+
+let y =
+    0
+    |>
+    run (state {
+        let! x = getEnv
+        let! y = ret "hi"
+        let! f = { foo = 0 }
+        printfn "%i" (x + 1)
+        printfn "%s" (y)
+        printfn "%A" f
+        custom "hi"
+        return x
+    })
