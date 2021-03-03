1291ae9434e7efa6b4ba82c1f9f7cb81589e1d55
Steven Sun <gsuuon+dev@gmail.com>
2021-03-03 02:32:47 -0500

    Update scratch inline, srtp

--- Scratch/Inline.fsx
+++ Scratch/Inline.fsx
@@ -1,6 +1,30 @@
 type MyType(name: string) =
-    member inline _.Name = name
+    member val Name = name
+
+    member inline x.PrintNameMember =
+        printfn "%A" x.Name
+        // Works
+
+    (* member inline _.PrintName = *)
+    (*     printfn "%A" name *)
+        // error FS1113: The value 'PrintName' was marked inline but its implementation makes use of an internal or private function which is not sufficiently accessible
 
 let t = MyType("hi")
 
 printfn "%A" t.Name
+
+/// But with SRTP, we get:
+
+(* type Foo< ^T when ^T :> obj>(name: string) = *)
+(*     member val Name = name *)
+        // error FS0670: This code is not sufficiently generic. The type variable  ^T could not be generalized because it would escape its scope.
+
+type FooBase(name: string) =
+    member val Name = name
+
+type Foo2< ^T when ^T :> obj>(name: string) =
+    inherit FooBase(name)
+
+    member inline x.DoThing() =
+        printfn "%A" x.Name
+            // Works
--- Scratch/Srtp.fsx
+++ Scratch/Srtp.fsx
@@ -1,8 +1,10 @@
 // from https://github.com/fsharp/fslang-suggestions/issues/641
 
 // I can specify a group of SRTP constraints like this:
-type DuckLike< ^a when ^a: (member Walk: unit -> unit)
-               and ^a: (member Quack: unit -> unit) > = ^a
+type DuckLike< ^a when ^a : (member Walk: unit -> unit)
+               and ^a : (member Quack: unit -> unit)
+               and ^a : (new: unit -> ^a)
+               > = ^a
 
 type Duck() =
     member _.Float() = printfn "Duck floating"
@@ -23,6 +25,31 @@ let inline doDuckLikeThings (duck: ^a DuckLike) =
     (^a : (member Walk : unit -> unit) (duck))
     (^a : (member Quack : unit -> unit) (duck))
 
+type DuckMarker< ^a when ^a : (member Walk: unit -> unit)
+               and ^a : (member Quack: unit -> unit)
+               and ^a : (new: unit -> ^a) >() = class end
+
+let inline createDuckLike< 'T, 'a when 'T :> DuckMarker<'a> > () = new ^a()
+(* type DuckContainer< 'T, 'a when 'T :> DuckMarker< 'a> > = { *)
+(*     // T_T why does the above work but this no.. *)
+(*     foo : string *)
+(*     duck : ^T *)
+(* } *)
+
+type DuckContainer2<'T> =
+  { foo : string
+    duck : 'T }
+    (* static member Empty () = *) // doesn't work
+    (*   { foo = "" *)
+    (*     duck = createDuckLike<_, 'T>() } *)
+
+let inline createDuckContainer2< 'T, 'a when 'T :> DuckMarker<'a> > () =
+  { foo = ""
+    duck = new 'a() }
+
+let inline createDuckContainer<'T>() =
+    createDuckContainer2<_, 'T>()
+
 let doDuckThings (duck: #Duck) =
     duck.Float()
 
@@ -34,13 +61,22 @@ let main () =
     let daffy = DaffyDuck()
     let donald = DonaldDuck()
 
+    let daffy2 = createDuckLike<_, DaffyDuck>()
+
     doDuckLikeThings daffy
     doDuckLikeThings donald
+    doDuckLikeThings daffy2
 
     daffy.Gloat()
     donald.Yell()
+    daffy2.Quack()
 
     doDuckThings(daffy)
     doDuckThings(donald)
+    doDuckThings(daffy2)
+
+    let duckContainer2 = createDuckContainer2<_, DaffyDuck>()
+
+    duckContainer2.duck.Quack()
 
 main()
