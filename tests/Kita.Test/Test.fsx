2effa12615ae16be65dbde3fd625d0fcce117a88
Steven Sun <gsuuon+dev@gmail.com>
2021-03-03 22:09:26 -0500

    Fix references in Test.fsx

--- tests/Kita.Test/Test.fsx
+++ tests/Kita.Test/Test.fsx
@@ -1,6 +1,10 @@
-#r "../bin/Release/netstandard2.0/Kita.dll"
+#I "bin/Release/netcoreapp3.1/"
+#r "Kita"
+#r "Kita.Test"
+#r "Kita.Providers"
+#r "Kita.Providers.Azure"
+#r "Kita.Providers.Local"
 
-open Kita.Test
 open Kita.Operations
 open Kita.Test.Examples
 
