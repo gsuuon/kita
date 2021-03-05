ac17659ea83b7f007d261d9a710746d96e26682b
Steven Sun <gsuuon+dev@gmail.com>
2021-03-04 21:21:28 -0500

    Update Stacktrace scratch

--- tests/Kita.Test/Scratch/Stacktrace.fsx
+++ tests/Kita.Test/Scratch/Stacktrace.fsx
@@ -13,3 +13,18 @@ let callFn fn x =
 
 
 let st = callFn myfn 0
+
+#if PRETEND_THIS_IS_IN_INFRA_BIND
+let inBindMemberOfInfra =
+    let getTrace n = 
+        let st = System.Diagnostics.StackTrace true
+        let frame = st.GetFrame n
+
+        sprintf "%A: %A"
+        <| frame.GetMethod().DeclaringType.
+            // "Kita.Test.Examples+cloudAbout@14-1"
+            // Good enough for closures
+        <| frame.GetFileLineNumber()
+
+    print s "Handled by" (getTrace 2)
+#endif
