dc9b99cf27ddf9d19e83c6f734e874452c6172c2
Steven Sun <gsuuon+dev@gmail.com>
2021-03-11 14:42:32 -0500

    Queue app

--- tests/Kita.Providers.Azure.Test/PulumiPrototype/QueueApp.fs
+++ tests/Kita.Providers.Azure.Test/PulumiPrototype/QueueApp.fs
@@ -2,17 +2,26 @@ module PulumiPrototype.Test.Queue
 
 open Kita.Core.Http
 open Kita.Core.Infra
-open Kita.Resources
+open Kita.Resources.Collections
 open Kita.Providers
 
 let kitaApp = infra'<Azure>
 
 let useQueue = kitaApp "basicQueue" {
-    let! q = CloudQueue()
+    let! q = CloudQueue<string>()
 
-    route "add_item" [
-        GET
+    route "item" [
+        POST
         <| fun req ->
-            q.
+            async {
+                do! q.Enqueue req.body
+                return { status = OK; body = "Got it" }
+            }
+        GET
+        <| fun _ ->
+            async {
+                let! x = q.Dequeue()
+                return { status = OK; body = x }
+            }
     ]
 }
