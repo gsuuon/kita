9d6e618ecdd2e936d2722cbf7ee34c5ec383d271
Steven Sun <gsuuon+dev@gmail.com>
2021-03-11 14:14:06 -0500

    Add Default.fs, fix Azure fsproj
    Having an empty reference collection project produces an fcs error.

---
+++ src/Kita.Providers/Default.fs
@@ -0,0 +1,8 @@
+module Kita.Providers.Default
+
+open Kita.Providers
+open Kita.Providers
+
+// Default configuration options can go here
+type Azure = Kita.Providers.Azure
+type Local = Kita.Providers.Local
