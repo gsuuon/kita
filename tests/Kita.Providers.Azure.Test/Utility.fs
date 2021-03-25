6c317b94b0e564c48a595c6add2ad2563ce8d2b3
Steven Sun <gsuuon+dev@gmail.com>
2021-03-25 08:40:01 -0500

    Task AwaitEvent, Waiter.Follow

--- tests/Kita.Providers.Azure.Test/Utility.fs
+++ tests/Kita.Providers.Azure.Test/Utility.fs
@@ -1,6 +1,16 @@
 module Kita.Utility
 
+open System.Threading.Tasks
+open FSharp.Control.Tasks
+
+type Task with
+    static member AwaitEvent (ev: IEvent<_>) =
+        let task = TaskCompletionSource<_>()
+        ev.Add <| fun x -> task.SetResult x
+        task.Task
+
 type Waiter<'T>() =
+        
     let mutable item : 'T option = None
 
     let onSet = new Event<'T>()
@@ -8,10 +18,12 @@ type Waiter<'T>() =
     [<CLIEvent>]
     member _.OnSet = onSet.Publish
     member _.Set x =
+        // TODO
+        // What should happen if already set? Error?
         item <- Some x
         onSet.Trigger x
 
-    member this.Get () =
+    member this.GetAsync =
         async {
             match item with
             | Some x ->
@@ -19,3 +31,16 @@ type Waiter<'T>() =
             | None ->
                 return! Async.AwaitEvent this.OnSet
         }
+
+    member this.GetTask =
+        task {
+            match item with
+            | Some x ->
+                return x
+            | None ->
+                return! Task.AwaitEvent this.OnSet
+        }
+
+    member this.Follow(waiter: Waiter<'R>, transform: 'R -> 'T) =
+        waiter.OnSet.Add <| fun (x: 'R) -> transform x |> this.Set
+        this
