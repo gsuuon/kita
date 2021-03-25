a6bca72b506ab12a7b60d641cb3913349c4b16a8
Steven Sun <gsuuon+dev@gmail.com>
2021-03-25 08:39:52 -0500

    Provider deploy draft

--- tests/Kita.Providers.Azure.Test/NativePrototype/Provider.fs
+++ tests/Kita.Providers.Azure.Test/NativePrototype/Provider.fs
@@ -4,34 +4,84 @@ open System
 
 open FSharp.Control.Tasks
 
+open Kita.Core
 open Kita.Providers
 open Kita.Utility
 
 open Provision
+
 open AzureNextApi
+open AzurePreviousApi
 
 type AzureNative() =
     inherit Provider("Azure.Native")
     let defaultLocation = "eastus"
 
-    let mutable deployRequests = []
-    let requestDeploy deploy =
-        // Could instantly deploy resource if provider already deployed
-        // to support attaching resources after deployment
-        deployRequests <- deploy :: deployRequests
+    let mutable provisionRequests = []
+    let requestProvision provision =
+        provisionRequests <- provision :: provisionRequests
 
     let connectionString = Waiter<string>()
     member _.WaitConnectionString = connectionString
     member _.OnConnection = connectionString.OnSet
-    member _.Deploy appName = task {
-        let! (rgName, saName, conString) =
-            provision appName defaultLocation
+    member _.Deploy
+        appName
+        location
+        (managed: Managed<AzureNative>)
+        = task {
 
-        for deploy in deployRequests do
-            do! deploy rgName saName
+        let! rg = Resources.createResourceGroup appName location
+        let! sa = Storage.createStorageAccount appName location
+
+        let rgName = rg.Name
+        let saName = sa.Name
+
+        let! key = Storage.getFirstKey rgName saName
+        let conString = Storage.formatKeyToConnectionString key saName
+
+        for provision in provisionRequests do
+            do! provision rgName saName
 
         connectionString.Set conString
-    }
+
+        let! appPlan = AppService.createAppServicePlan appName rgName
+
+        let! functionApp =
+            AppService.createFunctionApp
+                appName
+                appPlan
+                rgName
+
+        let! data = GenerateProject.generateFunctionsAppZip managed
+
+        let! blobContainerClient =
+            Blobs.blobContainerClient "deploy-zips-azure"
+
+        let blobClient = blobContainerClient.GetBlobClient("latest-deploy")
+
+        let! info =
+            use mem = new System.IO.MemoryStream(data)
+            blobClient.UploadAsync(mem) |> rValue
+
+        let! blobUri =
+            Blobs.blobGenerateSas
+                Blobs.Permission.Read
+                1.0
+                blobClient
+
+
+        (* let! deployment = *)
+        (*     functionApp *)
+        (*         .Deploy() *)
+        (*         .WithPackageUri(blobUri.AbsoluteUri) *)
+        (*         .WithExistingDeploymentsDeleted(false) *)
+        (*         .ExecuteAsync() *)
+
+        printfn "Blob sas uri: %s" blobUri.AbsoluteUri
+
+        return ()
+
+        }
         
     member _.RequestQueue (qName) =
-        requestDeploy <| Storage.createQueue qName
+        requestProvision <| Storage.createQueue qName
