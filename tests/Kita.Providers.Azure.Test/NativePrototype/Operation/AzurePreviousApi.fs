4852ae5257cb63a73eee834ca7718738c82f0702
Steven Sun <gsuuon+dev@gmail.com>
2021-03-25 08:40:59 -0500

    Small restructure of AzurePreviousApi

--- tests/Kita.Providers.Azure.Test/NativePrototype/Operation/AzurePreviousApi.fs
+++ tests/Kita.Providers.Azure.Test/NativePrototype/Operation/AzurePreviousApi.fs
@@ -17,19 +17,21 @@ open Microsoft.Azure.Management.ResourceManager.Fluent.Authentication
 
 open FSharp.Control.Tasks
 
-let credential =
-    let x = new AzureCredentialsFactory()
-    x.FromServicePrincipal
-        ( Environment.GetEnvironmentVariable
-            "AZURE_CLIENT_ID"
-        , Environment.GetEnvironmentVariable
-            "AZURE_CLIENT_SECRET"
-        , Environment.GetEnvironmentVariable
-            "AZURE_TENANT_ID"
-        , AzureEnvironment.AzureGlobalCloud
-        )
+[<RequireQualifiedAccess>]
+[<AutoOpen>]
+module Credential =
+    let credential =
+        let x = new AzureCredentialsFactory()
+        x.FromServicePrincipal
+            ( Environment.GetEnvironmentVariable
+                "AZURE_CLIENT_ID"
+            , Environment.GetEnvironmentVariable
+                "AZURE_CLIENT_SECRET"
+            , Environment.GetEnvironmentVariable
+                "AZURE_TENANT_ID"
+            , AzureEnvironment.AzureGlobalCloud
+            )
 
-let createAzure () =
     let azure =
         Azure
             .Configure()
@@ -37,40 +39,37 @@ let createAzure () =
             .WithSubscription(AzureNextApi.Credential.subId())
 
     printfn "Using subscription: %s" azure.SubscriptionId
-    azure
 
-let createFunctionApp
-    appName
-    (appPlan: IAppServicePlan)
-    (rgName: string)
-    (azure: IAzure)
-    = task {
-    // TODO set app service plan?
-    // app service plan should be
-    //   tier = "Dynamic"
-    //   Name = "Y1"
-    let! functionApp =
-        azure.AppServices.FunctionApps
-            .Define(appName)
-            .WithExistingAppServicePlan(appPlan)
-            .WithExistingResourceGroup(rgName)
-                // TODO
-                // create a resourceGroup
-                // set plan for resource group to be consumption
-            .CreateAsync()
+module AppService = 
+    let createFunctionApp
+        appName
+        (appPlan: IAppServicePlan)
+        (rgName: string)
+        = task {
 
-    printfn "Created functionApp: %s on storage: %s"
-        functionApp.Name
-        functionApp.StorageAccount.Name
+        let! functionApp =
+            azure.AppServices.FunctionApps
+                .Define(appName)
+                .WithExistingAppServicePlan(appPlan)
+                .WithExistingResourceGroup(rgName)
+                    // TODO
+                    // create a resourceGroup
+                    // set plan for resource group to be consumption
+                .CreateAsync()
 
-    return functionApp
-}
+        printfn "Created functionApp: %s on storage: %s"
+            functionApp.Name
+            functionApp.StorageAccount.Name
 
-let createAppServicePlan
-    appServicePlanName
-    (rgName: string)
-    (azure: IAzure)
-    = task {
+        return functionApp
+
+        }
+
+    let createAppServicePlan
+        appServicePlanName
+        (rgName: string)
+        = task {
+
         let! appServicePlan =
             azure.AppServices.AppServicePlans
                 .Define(appServicePlanName)
@@ -80,41 +79,24 @@ let createAppServicePlan
                 .CreateAsync()
 
         printfn "Using app service plan %s" appServicePlan.Name
+
         return appServicePlan
-    }
 
-let createBlobContainer containerName rgName saName (azure: IAzure) = task {
-    let! blobContainer = 
-        azure.StorageAccounts.Manager.BlobContainers
-            .DefineContainer(containerName)
-            .WithExistingBlobService(rgName, saName)
-            .WithPublicAccess(PublicAccess.None)
-            .CreateAsync()
-    
-    printfn "Using blob container: %s" blobContainer.Id
-    return blobContainer
-}
+        }
 
-let deployFunctionApp (fApp: IFunctionApp) (azure: IAzure) = task {
-    // I may need to just upload a blob, there doesn't seem to be an sdk way for zip deployment
-    // I can curl and PUT to the endpoint
-    // https://docs.microsoft.com/en-us/azure/azure-functions/deployment-zip-push#rest
-    // or with az cli
-    // https://docs.microsoft.com/en-us/azure/azure-functions/deployment-zip-push#cli
-    // blob deploy generates untracked garbage. I can stick it all into a specific blob/storage account?
-    // potentially useful as you could restore to previous state
-    // but that could actually cause your application to be in an inconsistent state with other app components
-    // I could use the same blob, overwrite it here, then use it so that the garbage doesn't accumulate?
-    let blobUrl = "pretend this is the blob url"
-    let! deployment =
-        fApp
-            .Deploy()
-            .WithPackageUri(blobUrl)
-            .WithExistingDeploymentsDeleted(false)
-            .ExecuteAsync()
+    let deployFunctionApp
+        blobUri
+        (functionApp: IFunctionApp)
+        = task {
 
-    if deployment.Complete then
-        printfn "Finished deployment: %s" deployment.Key
+        let! deployment =
+            functionApp
+                .Deploy()
+                .WithPackageUri(blobUri)
+                .ExecuteAsync()
 
-    return deployment
-}
+        printfn "Deployed %s with %s" functionApp.Name blobUri
+
+        return ()
+
+        }
