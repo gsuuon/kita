7e8bbe5a5c630823e6e03f4a4c646ea3f1203c38
Steven Sun <gsuuon+dev@gmail.com>
2021-03-29 13:37:02 -0500

    Remove Giraffe from App

--- tests/Kita.Providers.Azure.Test/NativePrototype/App/Program.fs
+++ tests/Kita.Providers.Azure.Test/NativePrototype/App/Program.fs
@@ -7,8 +7,6 @@ open Kita.Core.Infra
 open Kita.Core.Http
 open Kita.Core.Http.Helpers
 
-open GiraffePrototype.Program
-
 module App = 
     let azure = infra'<AzureNative>
 
@@ -34,9 +32,35 @@ module App =
         ]
     }
 
+    let app1 = azure "myaznativeapp" {
+        let! q = Resource.Queue<string>("myaznatq")
+
+        route "hi" [
+            GET <| fun _ -> async {
+                let! xs = q.Dequeue 20
+                return
+                    sprintf "Got %A" xs
+                    |> ok
+            }
+
+            POST <| fun req -> async {
+                let text =
+                    req.body
+                    |> Seq.toArray
+                    |> Text.Encoding.UTF8.GetString
+                do! q.Enqueue text
+                return ok "Ok sent"
+            }
+        ]
+    }
+
+    let combined = app >> app1
+
     let deploy () =
+        (* AzureNative.Run("myaznativeapp", "eastus", app) *)
+
         Managed.empty()
-        |> app
+        |> combined
         |> fun managed ->
             AzureNative.Run("myaznativeapp", "eastus", managed)
             |> Async.AwaitTask
@@ -51,7 +75,5 @@ let main argv =
 
         App.deploy()
         |> Async.RunSynchronously
-        |> fun managed ->
-            Server.start managed.handlers
 
     0 // return an integer exit code
--- tests/Kita.Providers.Azure.Test/NativePrototype/FunctionApp/ProxyApp/Program.fs
+++ tests/Kita.Providers.Azure.Test/NativePrototype/FunctionApp/ProxyApp/Program.fs
@@ -3,8 +3,6 @@ open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Azure.Functions.Worker.Configuration
 
-open Giraffe
-
 [<EntryPoint>]
 let main argv =
     let host = new HostBuilder()
