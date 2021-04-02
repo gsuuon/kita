29eaa77ad0407f6a36fe912f17be8e65ce12240e
Steven Sun <gsuuon+dev@gmail.com>
2021-04-02 03:14:41 -0500

    addNested casts to interface

--- src/Kita/Core/Infra/State.fs
+++ src/Kita/Core/Infra/State.fs
@@ -1,6 +1,7 @@
 namespace Kita.Core
 
 open Kita.Resources
+open Kita.Providers
 
 type State<'a, 'b, 'c> = State of (Managed<'b> -> 'a * Managed<'c>)
 type Resource<'T when 'T :> CloudResource> = Resource of 'T
@@ -17,9 +18,17 @@ module State =
         { state with
               handlers = state.handlers @ pathHandlers }
 
-    let addNested child parent =
+    let addNested (child: Managed<#Provider>) parent =
+        let c =
+            { handlers = child.handlers
+              resources = child.resources
+              name = child.name
+              nested = child.nested
+              provider = child.provider :> Provider
+            }
+
         { parent with
-            nested = parent.nested.Add (child.name, child) }
+            nested = parent.nested.Add (child.name, c) }
 
     let setName name managed =
         { managed with name = name }
