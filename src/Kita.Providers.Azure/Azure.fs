acd6df66f662915a617c9dfb827986079397bb39
Steven Sun <gsuuon+dev@gmail.com>
2021-04-03 00:57:41 -0500

    Update Provider interface placeholders

--- src/Kita.Providers.Azure/Azure.fs
+++ src/Kita.Providers.Azure/Azure.fs
@@ -1,9 +1,10 @@
 ﻿namespace Kita.Providers
 
-open Kita.Providers
+open Kita.Core
 
 type Azure() =
     interface Provider with
         member _.Name = "Azure.Default"
-        member _.Initialize() = printfn "Initialize Azure.Default"
+        member _.Launch(name, loc) =
+            printfn "Launching Azure.Default"
 
--- src/Kita.Providers.Local/Local.fs
+++ src/Kita.Providers.Local/Local.fs
@@ -1,13 +1,13 @@
 namespace Kita.Providers
 
-open Kita.Providers
+open Kita.Core
 
 type Local() =
     member _.Initialize() = printfn "Initialize Local.Default"
 
     interface Provider with
         member _.Name = "Local.Default"
-        member this.Initialize() = 
-            this.Initialize()
+        member _.Launch(name, loc) =
+            printfn "Launching Local.Default"
 
 
