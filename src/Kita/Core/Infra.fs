a1d3991bc10bba9c265e10d66695476294be0709
Steven Sun <gsuuon+dev@gmail.com>
2021-04-03 00:49:08 -0500

    Move Infra/* into Infra.fs, refactor for nesting - wip

--- src/Kita/Core/Infra.fs
+++ src/Kita/Core/Infra.fs
@@ -1,8 +1,57 @@
-namespace Kita.Core
+namespace rec Kita.Core
 
-open Kita.Providers
-open Kita.Resources
 open Kita.Core.Http
+
+type Provider =
+    abstract member Name : string
+    abstract member Launch : string * string -> unit
+
+module Ops =
+    let inline attach< ^C, ^R when ^R: (member Attach : ^C -> unit) and ^C :> Provider>
+        (
+            resource: ^R,
+            config: ^C
+        )
+        =
+        (^R: (member Attach : ^C -> unit) (resource, config))
+
+type CloudResource = class end
+
+type Managed<'Provider> =
+    { resources: CloudResource list
+      handlers: MethodHandler list
+      name: string
+      provider: 'Provider
+      nested : Map<string, unit -> Provider> }
+
+module Managed =
+    let inline empty<'Provider when 'Provider :> Provider> ()
+        =
+        { resources = []
+          handlers = []
+          name = ""
+          provider = System.Activator.CreateInstance<'Provider>()
+          nested = Map.empty }
+
+type State<'a, 'b, 'c> = State of (Managed<'b> -> 'a * Managed<'c>)
+type Resource<'T when 'T :> CloudResource> = Resource of 'T
+
+[<AutoOpen>]
+module State =
+    let addResource (resource: #CloudResource) state =
+        { state with
+              resources = resource :> CloudResource :: state.resources }
+
+    let getResources = State(fun s -> s.resources, s)
+
+    let addRoutes pathHandlers state =
+        { state with
+              handlers = state.handlers @ pathHandlers }
+
+    let setName name (managed: Managed<_>) =
+        { managed with name = name }
+
+    let ret x = State(fun s -> x, s)
 
 [<AutoOpen>]
 module Helper =
@@ -14,7 +63,11 @@ module Helper =
         <| label
         <| item
 
-type Infra< ^Provider when ^Provider :> Provider and ^Provider: (new : unit -> ^Provider)>
+type IBlock<'P> =
+    abstract member Name : string
+    abstract member Attach : Managed<'P> -> Provider
+
+type Infra< ^Provider when ^Provider :> Provider>
     (
         name: string
     ) =
@@ -64,11 +117,42 @@ type Infra< ^Provider when ^Provider :> Provider and ^Provider: (new : unit -> ^
 
             s |> m
 
-    member inline x.Run(State m) : Managed<'a> -> Managed< ^Provider > =
-        fun s ->
-            print s "run" ""
+    member inline x.Run(State runner) =
+        { new IBlock< ^Provider> with
+            member _.Name = x.Name
+            member this.Attach initState =
+                print initState "run" ""
 
-            s |> setName x.Name |> m |> snd
+                let ranState =
+                    initState
+                    |> setName x.Name
+                    |> runner
+                    |> snd
+
+                let nestedProviders =
+                    ranState.nested
+                    |> Map.map (fun k v ->
+                        let provider = v()
+                        printfn "Got child provider: %s" provider.Name
+                        provider
+                        )
+
+                { new Provider with
+                    member _.Name = x.Name
+                    member _.Launch (a, b) =
+                        let provider : 'Provider = ranState.provider
+
+                        printfn "Nested providers: %i" nestedProviders.Count
+
+                        nestedProviders
+                        |> Map.iter (fun k v ->
+                            printfn "Launching child: %s" v.Name
+                            v.Launch(a, b)
+                            )
+
+                        provider.Launch (a,b)
+                }
+        }
 
     [<CustomOperation("route", MaintainsVariableSpaceUsingBind = true)>]
     member inline _.Route
@@ -117,9 +201,7 @@ type Infra< ^Provider when ^Provider :> Provider and ^Provider: (new : unit -> ^
 
             let resource = creator resourceDef
 
-
             ctx, s |> addResource resource
-
 
     [<CustomOperation("nest", MaintainsVariableSpaceUsingBind = true)>]
     member inline _.Nest
@@ -132,12 +214,20 @@ type Infra< ^Provider when ^Provider :> Provider and ^Provider: (new : unit -> ^
         <| fun state ->
             let (ctx, s) = m state
 
-            let runNested = getNested ctx
-            
-            let nested = Managed.empty() |> runNested
+            let childApp : IBlock< 'T> = getNested ctx
+
+            printfn "Adding child: %s" childApp.Name
 
             ctx,
-            addNested nested s
+            { s with
+                nested =
+                    s.nested.Add
+                        ( childApp.Name
+                        , fun () ->
+                            Managed.empty<'T>()
+                            |> childApp.Attach
+                            ) }
+
 and Named(name: string) =
     // Gets around issues with inline accessing private data and SRTP:
     // error FS0670: This code is not sufficiently generic. The type variable  ^Provider when  ^Provider :> Config and  ^Provider : (new : unit ->  ^Provider) could not be generalized because it would escape its scope
@@ -146,9 +236,7 @@ and Named(name: string) =
 
 
 module Infra =
-    let inline infra'< ^Provider
-                            when ^Provider :> Provider
-                            and ^Provider: (new : unit -> ^Provider)>
+    let inline infra'< ^Provider when ^Provider :> Provider>
         name
         =
         Infra< ^Provider>(name)
