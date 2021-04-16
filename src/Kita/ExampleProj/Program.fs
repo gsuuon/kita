12e4dbe4c1a336efd3f6632fb2043d0c4c22d35d
Steven Sun <gsuuon+dev@gmail.com>
2021-04-15 21:28:54 -0500

    Update ExampleProj

--- src/Kita/ExampleProj/Program.fs
+++ src/Kita/ExampleProj/Program.fs
@@ -6,28 +6,21 @@ open Kita.Compile
 
 let mutable root = Unchecked.defaultof<AttachedBlock>
 
-let launch prov (name, loc, block) =
-    let path = 
-        try
-            Reflect.getStaticAccessPath block
-        with e ->
-            sprintf "Couldn't get path: %A" e
-
-    printfn "Launched %s: %s" prov path
+let sayLaunched name _ = printfn "Launched %s" name
 
 type AProvider() =
     interface Provider with
         member _.Launch(name, loc, block) =
-            launch "A" (name, loc, block)
+            sayLaunched "A" (name, loc, block)
 
 type BProvider() =
     interface Provider with
         member _.Launch(name, loc, block) =
-            launch "B" (name, loc, block)
+            sayLaunched "B" (name, loc, block)
 
 module App =
-    let aApp = infra'<AProvider> "myaapp"
-    let bApp = infra'<BProvider>
+    let aApp = infra' ("myaapp", AProvider())
+    let bApp name = infra' (name, BProvider())
 
     let bBlock = bApp "say hello" {
         route "hello" [
@@ -64,7 +57,8 @@ let program = App.root true
 
 [<EntryPoint>]
 let main argv =
-    root <- program.Attach(Managed.empty())
-    root.launch("my app", "earth")
+    Managed.empty()
+    |> program.Attach
+    |> launch "my app" "earth"
 
     0
