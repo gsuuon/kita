f90a5d40aab97de3d53645c81f5c74e72b7fb79e
Steven Sun <gsuuon+dev@gmail.com>
2021-05-06 17:58:29 -0500

    Run Block scenarios

--- Scratch/BlockRewrite/Program.fs
+++ Scratch/BlockRewrite/Program.fs
@@ -1,13 +1,6 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
-open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+open BlockRewrite
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    Program.SimpleScenario.go()
+    0
