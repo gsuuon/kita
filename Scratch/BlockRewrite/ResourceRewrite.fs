419bd00505e7530fb45ea0944e7c41b4ef3d5c38
Steven Sun <gsuuon+dev@gmail.com>
2021-05-13 22:10:25 -0500

    Add stateful approach (current implementation)

--- Scratch/BlockRewrite/ResourceRewrite.fs
+++ Scratch/BlockRewrite/ResourceRewrite.fs
@@ -29,6 +29,7 @@ type IResourceBuilder<'P, 'A when 'P :> Provider > =
     abstract Build : 'P -> 'A
 
 type ResourceBuilderMarker = interface end
+type ResourceStatefulMarker = interface end
 
 type AnotherProvider() =
     interface Provider with
@@ -47,6 +48,16 @@ type ResourceBlockBuilder< ^P when 'P :> Provider>(provider: 'P) = // Just using
         ) =
         let x = ( ^B : (member Build : 'P -> 'A) (builder, provider) )
         f x
+
+    // SRTP stateful resource
+    member inline _.Bind
+        (
+            resource : ^B when 'B :> ResourceStatefulMarker
+                          and 'B : (member Attach : 'P -> unit),
+            f
+        ) =
+        ( ^B : (member Attach : 'P -> unit) (resource, provider) )
+        f resource
 
     // Interface
     member inline _.Bind
@@ -101,6 +112,49 @@ module MyResources =
                         member _.Value () = value }
         // Can't implement interfaces in type extensions, so would need to create an inheriting type
         // which implements the builder for new providers and change all instantiations to that type
+        //
+        // The frontend type can be anything, can change between provider types. If the type becomes unexpected, that should be a compile error
+        // so I don't think it's a big problem.
+        //
+        // It also leaves the frontend type to be more flexible (could be good or bad..)
+
+    // Stateful SRTP with no frontend type
+    type ValueResourceState<'A>(value: 'A) =
+        interface ResourceStatefulMarker
+
+        member _.Attach (_p: SomeProvider) =
+            // Do stateful attach stuff here
+            ()
+
+        member _.Attach (_p: OtherProvider) =
+            // Do stateful attach stuff here
+            ()
+
+        member _.Value () = value
+        // Stateful so possible to get into an invalid state 
+        //  - what if ValueResourceState instantiated, but value is called before attach?
+        //    - generally value access should be async and shouldn't assume liveliness. Result is indefinite pause.
+        //    - would be nice to make this into a compile-time problem by forcing two different types for launch and run cases
+        //    - frontend can only be constructed once the provider is known
+        //    - frontend can still stall on provider problems
+        //      - launch / run provider generic marker type?
+        //      e.g.:
+        
+    type ILaunchProvider<'A> =
+        abstract Launch : unit -> unit
+
+    type IRunProvider<'A> =
+        abstract Run : unit -> unit
+
+    type ExampleLaunchRunProviderSeparatedResourceBuilder<'A>(value: 'A) =
+        member _.Launch (_p: ILaunchProvider<SomeProvider>) =
+            // do launch things
+            ()
+
+        member _.Run (_p: IRunProvider<SomeProvider>) =
+            ValueResourceFrontend<'A>
+                { new ValueResourceBackend<_> with 
+                    member _.Value () = value }
 
     // SRTP
     type ValueResourceSRTP<'A>(value: 'A) =
@@ -168,3 +222,34 @@ module Scenario =
                 return ()
             }
 
+    module SRTPResourceStateful =
+        let works = 
+            ResourceBlockBuilder<_>(SomeProvider()) {
+                let! _x = ValueResourceState(0)
+                return ()
+            }
+
+        let works2 =
+            ResourceBlockBuilder<_>(OtherProvider()) {
+                let! _x = ValueResourceState(0)
+                return ()
+            }
+
+        let breaks =
+            ResourceBlockBuilder<_>(AnotherProvider()) {
+                let! _x = ValueResourceState(0)
+                return ()
+            }
+
+        let remove_conditionalScratch =
+            ResourceBlockBuilder<_>(SomeProvider()) {
+                let! _x = ValueResourceState(0)
+
+                let! _y =
+                    if _x.Value() > 0 then
+                        ValueResourceState(3)
+                    else
+                        ValueResourceState(3)
+
+                return ()
+            }
