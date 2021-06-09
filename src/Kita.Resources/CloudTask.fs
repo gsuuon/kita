4e8ad333cf7004583b15aad4a5f585dc42158213
Steven Sun <gsuuon+dev@gmail.com>
2021-06-09 16:56:22 -0500

    CloudTask resource rewrite drafts (notes)
    This was exploratory stuff, will be replaced immediately but I wanted
    the history here

--- src/Kita.Resources/CloudTask.fs
+++ src/Kita.Resources/CloudTask.fs
@@ -5,11 +5,80 @@ open System.Threading.Tasks
 open Kita.Core
 open Kita.Providers
 
-type CloudTaskFrontend(asyncWork: Async<unit>) =
-    interface CloudResource
-    
+type ResourceProvider<'Resource, 'a> =
+    abstract BuildResource : 'a -> 'Resource
+
+type ResourceBuilder<'Resource, 'a> =
+    abstract Build : ResourceProvider<'Resource, 'a> -> 'Resource
+
+
+module CloudTask =
+    type Frontend(asyncWork: Async<unit>) =
+        interface CloudResource
+        member _.Exec () = ()
+        member _.Stop () = ()
+
+type ICloudTask =
+    inherit CloudResource
+    abstract Exec : unit -> unit
+    abstract Stop : unit -> unit
+
+type ProviderActivatedCloudTask(work) =
+    interface ICloudTask with
+        member _.Exec () = ()
+        member _.Stop () = ()
+    interface Activate with
+        member _.Activate
+
+type FooProvider() =
+    member _.QueueProvisionProc work =
+        client.requestProvision worker work
+
+    interface ResourceProvider<CloudTask, Async<unit>> with
+        member this.Provision (work) =
+            this.QueueProvisionProc work
+
+type NCloudTask(work: Async<unit) =
+    interface ICloudTask with
+        member _.Exec () = ()
+        member _.Stop () = ()
+
+    interface ProviderProvision<ResourceProvider<ICloudTask, Async<unit>> with
+        member this.Provision provider =
+            provider.Provision this
+
 type CloudTask(asyncWork: Async<unit>) =
-    interface ResourceBuilder<Azure, CloudTaskFrontend> with
-        member _.Build _p =
-            printfn "Built Azure CloudTask"
-            CloudTaskFrontend(asyncWork)
+    interface ResourceBuilder<CloudTask.Frontend, Async<unit>> with
+        member _.Build (provider) = provider.BuildResource(asyncWork)
+
+type MyProvider() =
+    interface ResourceProvider<CloudTask.Frontend, Async<unit>> with
+        member _.BuildResource work =
+            // queueProvisionCloudTask()
+            CloudTask.Frontend(work)
+
+type MyAddendumProvider() =
+    inherit MyProvider()
+
+    interface ResourceProvider<CloudTaskFrontend, Async<bool>> with
+        member _.BuildResource work =
+            let work' = async {
+                let! _x = work
+                return ()
+            }
+
+            CloudTaskFrontend(work')
+
+let x =
+    block "" {
+        let! cloudTask = CloudTask(asyncRet ())
+        cloudTask.Exec()
+        cloudTask.Stop()
+    }
+
+type Builder(provider) =
+    member _.Bind (
+        rb: ResourceBuilder<'Resource, 'a>,
+        f
+    ) =
+    let r = rb.Build provider
