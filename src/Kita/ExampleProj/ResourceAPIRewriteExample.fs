ae445a01c02c5fd4af134934cd3147f642920439
Steven Sun <gsuuon+dev@gmail.com>
2021-06-09 13:38:57 -0500

    Resource rewrite example (info in msg)
    Restructure of provider/resource relationship which gives us:
    
    - Each resource does not depend on all providers
        - This was the main reason for the rewrite. Runtime would be bloated
        without this because we'd be forced to import all providers, with
        their entire management and client libraries, for any program
        - This also makes extension, either adding new resources or
          providers, much easier
    
    - Providers do not need to support every resource
        - Same as before
    
    - Better error messages at compile time if a resource is not supported
        - We used to just get an error saying no Bind overload matches
        - Now it's FooProvider isn't compatible with XResourceProvider
    
    What it costs us:
    - 3 types per resource type
        - resource provider, constructor, frontend contract

---
+++ src/Kita/ExampleProj/ResourceAPIRewriteExample.fs
@@ -0,0 +1,142 @@
+module ExampleProj.ResourceAPIRewriteExample
+
+open Kita.Core
+
+// Frontend contract
+type IValResource<'T> =
+    inherit CloudResource
+    abstract Get : unit -> 'T
+    abstract Set : 'T -> unit
+
+// Backend constructor contract
+type ValResourceProvider =
+    abstract Provide : 'T -> IValResource<'T>
+
+// Captures constructor argument
+type ValResource<'T>(v: 'T) =
+    member _.Create (provider: #ValResourceProvider) =
+        provider.Provide v
+
+
+type ILogResource =
+    inherit CloudResource
+    abstract Log : string -> unit
+
+type LogResourceProvider =
+    abstract Provide : unit -> ILogResource
+
+type LogResource() =
+    member _.Create (provider: #LogResourceProvider) =
+        provider.Provide ()
+
+
+
+type FooProvider() =
+    interface Provider with
+        member _.Run () = ()
+        member _.Launch () = ()
+
+    interface ValResourceProvider with
+        member _.Provide arg =
+            let mutable v = arg
+            { new IValResource<'T> with
+                member _.Get () = v
+                member _.Set x = v <- x }
+
+    interface LogResourceProvider with
+        member _.Provide () =
+            { new ILogResource with member _.Log x = printfn "%s" x }
+
+
+let inline create< ^p, ^rc, ^r when 'rc : (member Create : 'p -> 'r)>
+    (resBuilder: 'rc)
+    (provider: 'p) : 'r
+    =
+    let resource = (^rc : (member Create: 'p -> 'r )(resBuilder, provider))
+
+    resource
+
+let tryCall () =
+    let provider = FooProvider()
+
+    let valBuilder = ValResource 0
+    let logBuilder = LogResource ()
+
+    let v = valBuilder.Create provider
+    let l = logBuilder.Create provider
+
+    let v' = create valBuilder provider
+    let l' = create logBuilder provider
+
+    ()
+
+type ResourceBlock< ^Provider>(provider: 'Provider) =
+    member inline _.Bind< ^rc, ^r when
+                                    'rc : (member Create : 'Provider -> 'r)>
+        ( rc: 'rc, f)
+        =
+        let resource = create rc provider
+        f resource
+
+    member inline _.Return x = x
+
+let blockA =
+    ResourceBlock(FooProvider()) {
+        let! x = ValResource 0
+
+        x.Set 1
+        let x' = x.Get
+
+        let! y = LogResource()
+
+        y.Log "Hi dere"
+
+        return x'
+    }
+
+type IQueueResource<'T> =
+    inherit CloudResource
+    abstract Queue : 'T -> unit
+    abstract Dequeue : unit -> 'T option
+
+type QueueResourceProvider =
+    abstract Provide : unit -> IQueueResource<'T>
+
+type QueueResource<'T>() =
+    member _.Create (provider: QueueResourceProvider) =
+        provider.Provide<'T> ()
+
+type FooQProvider() =
+    inherit FooProvider()
+
+    interface QueueResourceProvider with
+        member _.Provide<'T> () =
+            let mutable q = List.empty
+
+            { new IQueueResource<'T> with
+                member _.Queue x =
+                    q <- x :: q
+                member _.Dequeue () =
+                    match q with
+                    | head :: rest ->
+                        q <- rest
+                        Some head
+                    | [] ->
+                        None
+                    }
+
+let blockB =
+    ResourceBlock(FooQProvider()) {
+        let! x = ValResource 0
+        x.Set 1
+        let x' = x.Get
+
+        let! y = LogResource()
+
+        y.Log "Hi dere"
+
+        let! z = QueueResource<char> ()
+
+        return x'
+    }
+
